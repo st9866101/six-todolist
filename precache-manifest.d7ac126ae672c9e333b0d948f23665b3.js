9b1df5608d6f9abb60cb7eceab624719b721d5c6
st9866101 <st9866101@gmail.com>
2022-09-04 19:02:23 +0800

    Updates

---
+++ precache-manifest.d7ac126ae672c9e333b0d948f23665b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c3b246066e978d217f4",
+    "url": "/six-todolist/static/css/main.511074dd.chunk.css"
+  },
+  {
+    "revision": "0c3b246066e978d217f4",
+    "url": "/six-todolist/static/js/main.0c3b2460.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/six-todolist/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "7372631245bdf377fbdf",
+    "url": "/six-todolist/static/js/2.73726312.chunk.js"
+  },
+  {
+    "revision": "a11a6564ae46caad9a22",
+    "url": "/six-todolist/static/js/runtime~main.a11a6564.js"
+  },
+  {
+    "revision": "987320be68cdd3c4136559546c445b2a",
+    "url": "/six-todolist/index.html"
+  }
+];
